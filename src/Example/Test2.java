cadf11a80de88ba5a82932c86c6109fec0c41a22
pdchavan28 <55650815+pdchavan28@users.noreply.github.com>
2021-07-18 15:51:19 +0530

    Update Test2.java

--- src/Example/Test2.java
+++ src/Example/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		
+		// Test Fetch and Merge into Workspace from remote repositiry
 		System.out.println("Test File to commit into Master");
 
 	}
